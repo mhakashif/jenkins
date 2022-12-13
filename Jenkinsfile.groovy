35d693d4d40a19baf4b8aa7503aaae6d56d9133b
Kashif <Kashif@123>
2022-12-13 22:42:35 +0530

    job

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
     stages {
         stage('Stage 1') {
             steps {
-                echo 'myfirstjob Welcome19!' 
+                echo 'myfirstjob to this!' 
             }
         }
     }
